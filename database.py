aa661cc6425fdbfcecafc89fcaacd9b38aaf171e
CollinsKandie98 <kandieck17@gmail.com>
2025-03-05 21:02:33 +0300

    initial

--- database.py
+++ database.py
@@ -1,7 +1,8 @@
 import psycopg2
 
 # Connect to your postgres DB
-conn = psycopg2.connect(dbname="myduka", user="postgres",password="Pass@123!",port=5432)
+# conn = psycopg2.connect(host="134.209.24.19", dbname="collins", user="collins",password="12345",port=5432)
+conn = psycopg2.connect(host="localhost", port="5432", database="myduka", user="postgres", password="Pass@123!")
 
 # Open a cursor to perform database operations
 cur = conn.cursor()
--- main.py
+++ main.py
@@ -1,13 +1,20 @@
-from flask import Flask, render_template, request, redirect, flash
-from database import conn, cur
+from flask import Flask,render_template,request,redirect,flash,session,url_for
+from database import conn,cur
+from functools import wraps
+from flask_bcrypt import Bcrypt
+
 app = Flask(__name__)
-app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
+bcrypt = Bcrypt(app)
 
-cur.execute("CREATE TABLE IF NOT EXISTS products(id SERIAL PRIMARY KEY, name VARCHAR(100), buying_price NUMERIC(14,2),selling_price NUMERIC(14,2), stock_quantity INTEGER);")
+app.secret_key = '_5#y2L"F4Q8z\n\xebuhbivyc]/'
 
-cur.execute("CREATE TABLE IF NOT EXISTS sales(id SERIAL PRIMARY KEY, pid INTEGER REFERENCES products(id), quantity INTEGER NOT NULL, created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP);")
-
-conn.commit()
+def login_required(f):
+    @wraps(f)
+    def protected():
+        if 'email' not in session:
+            return redirect(url_for('login'))
+        return f()
+    return protected
 
 
 @app.route('/')
@@ -23,6 +30,26 @@ def about():
 @app.route('/contact-us')
 def contact():
     return render_template("contact.html")
+
+@app.route('/register', methods=["GET", "POST"])
+def register():
+    if request.method =="POST":
+
+        fullName = request.form["fullname"]
+        email = request.form["emailaddress"]
+        password = request.form["password"]
+        hash_password = bcrypt.generate_password_hash(password).decode('utf-8')
+
+        new_user_query ="INSERT INTO users(name,email_address,password)"\
+                        "VALUES('{}', '{}', '{}')".format(fullName,email, hash_password)
+        print("The hashed passowrd is" + hash_password)
+        
+        cur.execute(new_user_query)
+        conn.commit()
+
+        return redirect('/register')
+    else:
+        return render_template("register.html")
 
 
 @app.route('/products', methods=["GET", "POST"])
@@ -46,6 +73,7 @@ def products():
 
 
 @app.route("/sales", methods=["GET", "POST"])
+@login_required
 def sales():
     if request.method == "GET":
         cur.execute("select * from sales")
@@ -65,48 +93,49 @@ def sales():
 
 @app.route("/dashboard", methods=["GET", "POST"])
 def dashboard():
-    cur.execute("SELECT prodcuts.name AS product_name, SUM(sales.quantity * (products.selling_price - products.buying_price)) AS total_profit FROM sales JOIN products ON")
-    sales_result = cur.fetchall()
-    x = []
-    y = []
-    for i in sales_result:
-        x.append(i[0])
-        y.append(float(i[1]))
+    # cur.execute("SELECT prodcuts.name AS product_name, SUM(sales.quantity * (products.selling_price - products.buying_price)) AS total_profit FROM sales JOIN products ON")
+    # sales_result = cur.fetchall()
+    # x = []
+    # y = []
+    # for i in sales_result:
+    #     x.append(i[0])
+    #     y.append(float(i[1]))
 
-    cur.execute(
-        "select products.name as product_name, sum(sales.quantity * (products.selling.price - products.buying_price) as total_profit from sales")
-    profit_results = cur.fetchall()
+    # cur.execute(
+    #     "select products.name as product_name, sum(sales.quantity * (products.selling.price - products.buying_price) as total_profit from sales")
+    # profit_results = cur.fetchall()
 
-    return render_template("dasboard.html", x-x, y-y, profit_results-profit_results)
+    # return render_template("dasboard.html", x-x, y-y, profit_results-profit_results)
+    return render_template("dashboard.html")
 
 
-@app.route("/register", methods=["GET", "POST"])
-def register():
-    if request.method == "GET":
-        cur.execute("SELECT * FROM users")
-        users = cur.fetchall()
+# @app.route("/register", methods=["GET", "POST"])
+# def register():
+#     if request.method == "GET":
+#         cur.execute("SELECT * FROM users")
+#         users = cur.fetchall()
 
-        print("GET request received. users fecthed:", users)  # Debugging output
-        return render_template("register.html", users=users)
-    else:
-        # Get from data
-        full_name = request.form["name"]
-        email_address = request.form["email"]
-        password = request.form["Pass"]
+#         print("GET request received. users fecthed:", users)  # Debugging output
+#         return render_template("register.html", users=users)
+#     else:
+#         # Get from data
+#         full_name = request.form["name"]
+#         email_address = request.form["email"]
+#         password = request.form["Pass"]
 
-        # Inster user into database safely
-        query = """
-    INSERT INTO users (full_name,email_address,password)
-    values (%s, %s, %s) RETURNING id;
-    """
-        cur.execute(query, (full_name, email_address, password))
-        user_id = cur.fetchone()[0]  # Retrive inserted user ID
-        conn.commit()
+#         # Inster user into database safely
+#         query = """
+#     INSERT INTO users (full_name,email_address,password)
+#     values (%s, %s, %s) RETURNING id;
+#     """
+#         cur.execute(query, (full_name, email_address, password))
+#         user_id = cur.fetchone()[0]  # Retrive inserted user ID
+#         conn.commit()
 
-        print(f"User registered with ID: {user_id}")
+#         print(f"User registered with ID: {user_id}")
 
-        # Redirect back to \register after successful registration
-        return redirect("/register")
+#         # Redirect back to \register after successful registration
+#         return redirect("/register")
 
 
 @app.route("/login", methods=["GET", "POST"])
@@ -114,8 +143,7 @@ def login():
     if request.method == "POST":
         email_address = request.form["emailaddress"]
         password = request.form["password"]
-        querylogin = "select id from users where email_address = '{}' and password = '{}'".format(
-            email_address, password)
+        querylogin = "select id from users where email_address = '{}' and password = '{}'".format(email_address, password)
         cur.execute(querylogin)
         row = cur.fetchone()
 
@@ -123,9 +151,10 @@ def login():
             flash("Invalid credentials")
             return render_template("login.html")
         else:
-            return redirect("/dashboard")
+            session['email'] = email_address
+            return redirect(url_for('dashboard'))
     else:
         return render_template("login.html")
 
-
-app.run(debug=True)
+if __name__ == '__main__':
+    app.run(debug=True)
